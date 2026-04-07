e2aaa543d4d953335c0fcbe3906ab3d74525b56b
Bocman_Alex <bocman@example.com>
2026-04-07 09:43:48 +0000

    Lab3 final project

---
+++ build-and-push.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+REGISTRY="docker.io"
+NAMESPACE="botsman01"          # ЗАМЕНИТЕ НА ВАШ Docker Hub USERNAME
+IMAGE_NAME="student-app-api"
+VERSION=${VERSION:-$(git describe --tags --always --dirty 2>/dev/null || echo "v0.0.0")}
+
+echo "Building $REGISTRY/$NAMESPACE/$IMAGE_NAME:$VERSION"
+docker build -t $REGISTRY/$NAMESPACE/$IMAGE_NAME:$VERSION ./api
+docker push $REGISTRY/$NAMESPACE/$IMAGE_NAME:$VERSION
+echo "Published $VERSION"
